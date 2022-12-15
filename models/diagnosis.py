b77abd10ed59456e303e2ee93602e4cab8f908f5
Unai Zuluaga <zuluagaunai@gmail.com>
2022-12-15 09:57:08 +0100

    first classes done

---
+++ models/diagnosis.py
@@ -0,0 +1,25 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+# -*- coding: utf-8 -*-
+
+from odoo import api
+from odoo import fields
+from odoo import models
+
+class diagnosis(models.Model):
+    _name = 'aether.diagnosis'
+    
+    name = fields.Char(required = True)
+    diagnosisDate = fields.Date(required = True)
+    lastDiagnosisChangeDate = fields.Date(required = True)
+#    patient = fields.Many2one('res.users', ondelete='cascade',
+#   string="Patient",required = True)
+#    psychologist= fields.Many2one('res.users', ondelete='cascade',
+#   string="Psychologist",required = True)
+    mentalDisease = fields.Many2one('aether.mentaldisease', ondelete='cascade',
+                                    string="MentalDisease", required=True)
+    onTheraphy=current = fields.Boolean(string = "Active", required = True)
+    treatment=fields.OneToMany('aether.treatment','diagnosis',String="Treatment")
+
---
+++ models/treatment.py
@@ -0,0 +1,23 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+
+from odoo import api
+from odoo import fields
+from odoo import models
+
+class treatment(models.Model):
+    _name = 'aether.treatment'
+    
+    name = fields.Char(required=True)
+    medication = fields.Many2one('aether.medication', ondelete='cascade',
+                                 string="Medication", required=True)
+    diagnosis = fields.Many2one('aether.diagnosis', ondelete='cascade',
+                                string="Diagnosis", required=True)
+    day = fields.Selection([('MONDAY', 'MONDAY'), ('TUESDAY', 'TUESDAY'),
+                           ('WEDNESDAY', 'WEDNESDAY'), ('THURSDAY', 'THURSDAY'),
+                           ('FRIDAY', 'FRIDAY'), ('SATURDAY', 'SATURDAY')('SUNDAY', 'SUNDAY')], required=True)
+    
+    daytime = fields.Selection([('MORNING', 'MORNING'), ('NOON', 'NOON'),
+                               ('NIGHT', 'NIGHT')], required=True)                
