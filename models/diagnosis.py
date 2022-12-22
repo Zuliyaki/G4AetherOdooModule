571c261bd8eeb9952e43073f86e3d4a75bbcdbd3
Unai Zuluaga <zuluagaunai@gmail.com>
2022-12-22 08:44:40 +0100

    diagnosis changes made

--- models/diagnosis.py
+++ models/diagnosis.py
@@ -17,10 +17,10 @@ class diagnosis(models.Model):
     patient = fields.Many2one('res.users', ondelete='cascade',
                               string="Patient", required=True)
     psychologist = fields.Many2one('res.users', ondelete='cascade',
-                               string="Psychologist", required=True)
+                                   string="Psychologist", required=True)
     mentalDisease = fields.Many2one('aether.mentaldisease', ondelete='cascade',
                                     string="MentalDisease", required=True)
-    onTheraphy = current = fields.Boolean(string="Active", required=True)
+    onTherapy = fields.Boolean(string="Active", required=True)
 #    treatment=fields.OneToMany('aether.treatment','diagnosis',String="Treatment") para simplificar el model
 #esto seria la simplificacion del modelo, solo se trataria con una medicación, quitariamos días y horas del dia también
     medicationType = fields.Selection([('ANTIDEPRESSANTS', 'ANTIDEPRESSANTS'), ('ANTIANXIETYMEDICATIONS', 'ANTI ANXIETY MEDICATIONS'),
