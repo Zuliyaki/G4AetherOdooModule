1b35aafd7f54c3f224e1003fb46e4844ddde1b83
BBonLLo <65909299+BBonLLo@users.noreply.github.com>
2023-01-12 09:21:06 +0100

    View Daily notes

--- __manifest__.py
+++ __manifest__.py
@@ -27,6 +27,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
+        'views/viewsDailyNotes.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
