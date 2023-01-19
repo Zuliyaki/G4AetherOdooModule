9f01136abbd24e660209b6480f8f5f182673e3cd
BBonLLo <65909299+BBonLLo@users.noreply.github.com>
2023-01-19 10:14:00 +0100

    Data Model - Done but missing relation between psychologist and patient

--- models/patient.py
+++ models/patient.py
@@ -7,6 +7,6 @@ class patient(models.Model):
     
     diagnosis = fields.One2many('aether.diagnosis', 'patient', string="Diagnosis")
     dailynote = fields.One2many('aether.dailynote', 'dnPatient', string="Daily Note")
-    # psychologist = fields.Many2one('aether.psychologist', ondelete='cascade', string="Psychologist", required=True)
+    #psychologist = fields.Many2one('aether.psychologist', ondelete='cascade', string="Psychologist", required=True)
     appointment = fields.One2many('aether.appointments', 'patient', string ="Appointment")
     
