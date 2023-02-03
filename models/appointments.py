3d4cbb9a9930c0559587009a1d78c201025a0574
janam1003 <lucasjanamsmile@gmail.com>
2023-02-03 09:51:59 +0100

    views only

--- models/appointments.py
+++ models/appointments.py
@@ -7,10 +7,33 @@ from odoo import fields
 from odoo import models
 
 class appointment(models.Model):
+    
     _name = "aether.appointments"
+    
     name = fields.Char(required=True)
+    
     appointmentChange = fields.Boolean(string="AppointmentChange", required=True)
+    
     appointmentDate = fields.Date(string="AppointmentDate", required=True)
+    
     patient = fields.Many2one('res.users', ondelete='cascade', string="Patient", required=True)
+    
     psychologist = fields.Many2one('res.users', ondelete='cascade', string="Psychologist", required=True)
  
+ 
+    @api.onchange('Date', 'appointmentDate')
+    def _verify_Available_Dates(self):
+        if self.Date < today:
+            return {'warning': {
+                'title': "Incorrect 'Date' value", 
+                'message': "The Date is not available today.", },
+            }
+            
+            if self.Date < len(self.appointmentDate):
+                return {'warning': {
+                'title': "Too Many Dates", 
+                'message': "Please Change Date", },
+            }
+            
+            
+            
