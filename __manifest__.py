01cc34d8ccd923d4ae930dea5cc1315a6811f91b
BBonLLo <65909299+BBonLLo@users.noreply.github.com>
2023-02-03 09:51:57 +0100

    Daily Note Test DONE

--- __manifest__.py
+++ __manifest__.py
@@ -26,8 +26,8 @@
 
     # always loaded
     'data': [
-        'security/security.xml',
-        'security/ir.model.access.csv',
+        # 'security/security.xml',
+        # 'security/ir.model.access.csv',
         'views/menu.xml',
         'views/views.xml',
         'views/viewsDailyNotes.xml',
